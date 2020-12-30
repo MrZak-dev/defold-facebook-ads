07cdd8b1698425b4d9755417a8a13abdd6166f77
MrZak-dev <zakaria.guenna@gmail.com>
2020-12-30 16:41:58 +0300

    Ads implementation interstitial,rewarded, banner

--- src/com/b4dnetwork/extensions/JFacebookAds.java
+++ src/com/b4dnetwork/extensions/JFacebookAds.java
@@ -2,83 +2,281 @@ package com.b4dnetwork.extensions;
 
 import android.util.Log;
 import android.app.Activity;
-import android.content.Context;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.facebook.ads.*;
 
 
 public class JFacebookAds{
-    private String TAG = "defold";
+
+
+    private final String TAG = "defold";
     private Activity activity;
 
     private InterstitialAd FbInterstitialAd;
+    private RewardedVideoAd FbRewardedVideoAd;
+    private AdView AdView; // banner Ad
+
+
+
+    private LinearLayout linearLayout = null;
+    private LinearLayout.LayoutParams linearLayoutParams = null;
+    private WindowManager windowManager;
+    private WindowManager.LayoutParams windowManagerParams = null;
+
+
+    //Ads Signals
+    private boolean isInterstitialLoaded , isRewardedLoaded , isBannerShown , isBannerLoaded = false;
+
 
     public JFacebookAds(Activity activity){
         this.activity = activity;
     }
 
 
-    public String Public(String text){
-        return text;
-    }
 
     public void Initialize(Activity activity){
         AudienceNetworkAds.initialize(activity);
-        //Log.e("FBADS", "Initialize.............");
     }
 
-    public void LoadInterstitial(Activity activity, String placementId){
-        
+
+    /**
+     * Load a interstitial ad
+     * @param placementId interstitial ad
+     */
+    public void LoadInterstitial(String placementId){
+
         FbInterstitialAd = new InterstitialAd(activity,placementId);
-         
+
+        InterstitialAdListener interstitialAdListener = new InterstitialAdListener() {
+            @Override
+            public void onInterstitialDisplayed(final Ad ad) {
+                // Interstitial ad displayed callback
+                Log.i(TAG, "Interstitial ad displayed.");
+            }
+
+            @Override
+            public void onInterstitialDismissed(final Ad ad) {
+                // Interstitial dismissed callback
+                Log.i(TAG, "Interstitial ad dismissed.");
+                FbInterstitialAd.loadAd(); // load the interstitial , so we can use it again.
+            }
+            @Override
+            public void onError(final Ad ad, final AdError adError) {
+                // Ad error callback
+                Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(final Ad ad) {
+                // Interstitial ad is loaded and ready to be displayed
+                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!");
+                // switch loading signal to true
+                isInterstitialLoaded = true;
+
+            }
+            @Override
+            public void onAdClicked(final Ad ad) {
+                // Ad clicked callback
+                Log.i(TAG, "Interstitial ad clicked!");
+            }
+            @Override
+            public void onLoggingImpression(final Ad ad) {
+                // Ad impression logged callback
+                Log.i(TAG, "Interstitial ad impression logged!");
+            }
+        };
+
+        FbInterstitialAd.loadAd(
+                FbInterstitialAd.buildLoadAdConfig()
+                        .withAdListener(interstitialAdListener)
+                        .build());
+    }
+
+    /**
+     * Display a loaded Interstitial ad
+     */
+    public void ShowInterstitial(){
+        if(isInterstitialLoaded){
+            FbInterstitialAd.show();
+            isInterstitialLoaded = false; //reset the signal to false so the Adloaded signal from the ad listener will set it to true again when ad is loaded
+        }else{
+            Log.e(TAG,"Interstitial is not Loaded yet !");
+        }
+    }
+
+    /**
+     * Load a Facebook rewarded video ad
+     * @param placementId placement id
+     */
+    public void LoadRewardedVideo(String placementId){
+        FbRewardedVideoAd = new RewardedVideoAd(activity,placementId);
+
+        RewardedVideoAdListener rewardedVideoListener = new RewardedVideoAdListener() {
+            @Override
+            public void onRewardedVideoCompleted() {
+                //TODO : Call Reward function ,
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+
+            }
+
+            @Override
+            public void onRewardedVideoClosed() {
+                FbRewardedVideoAd.loadAd();
+            }
+
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                Log.e(TAG, "Rewarded ad failed to load: " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                isRewardedLoaded = true;
+                Log.d(TAG, "Rewarded Video ad is loaded and ready to be displayed!");
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+
+            }
+        };
+
+        FbRewardedVideoAd.loadAd(
+                FbRewardedVideoAd.buildLoadAdConfig()
+                        .withAdListener(rewardedVideoListener)
+                        .build());
+
+    }
+
+    /**
+     * Display a loaded Rewarded Video Ad
+     */
+    public void ShowRewardedVideo(){
+        if(isRewardedLoaded){
+            isRewardedLoaded = false;
+            FbRewardedVideoAd.show();
+        }
+    }
+
+
+    /**
+     *
+     * @param placementId Banner Placement Id
+     */
+    public void LoadBanner(String placementId){
+
+        AdView = new AdView(activity, placementId , AdSize.BANNER_HEIGHT_50);
+
+        AdListener adListener = new AdListener() {
+            @Override
+            public void onError(Ad ad, AdError adError) {
+                Log.e(TAG, "Banner Ad Failed : " + adError.getErrorMessage());
+            }
+
+            @Override
+            public void onAdLoaded(Ad ad) {
+                isBannerLoaded = true;
+                Log.d(TAG, "Banner ad is loaded !");
+            }
+
+            @Override
+            public void onAdClicked(Ad ad) {
+                // Ad clicked callback
+            }
+
+            @Override
+            public void onLoggingImpression(Ad ad) {
+                // Ad impression logged callback
+            }
+        };
+
+        Log.i(TAG,"Banner is loading...");
+
+        AdView.loadAd(AdView.buildLoadAdConfig().withAdListener(adListener).build());
+    }
+
+
+    /**
+     * Show Banner Ad
+     */
+    public void ShowBanner(){
+
+        if(!isBannerLoaded){
+            Log.e(TAG, "No Banner loaded to show : Call load_banner() first ");
+            return;
+        }
+
+        if(isBannerShown){
+            Log.e(TAG, "Banner already shown !");
+            return;
+        }
+
+        AddBannerToLayout();
+    }
+
+    /**
+     * hide Banner Ad
+     */
+    public void HideBanner(){
+
+        if(!isBannerLoaded){
+            Log.e(TAG, "No Banner loaded to hide : Call load_banner() first ");
+            return;
+        }
+
+        if(!isBannerShown){
+            Log.e(TAG, "Banner already Hidden !");
+            return;
+        }
+
+
+        isBannerShown = false;
+        isBannerLoaded = false;
+        windowManager.removeView(linearLayout);
+        linearLayout.removeView(AdView);
+        linearLayout = null;
+        AdView = null;
+
+        Log.i(TAG, "Banner hidden !");
+    }
+
+
+    private void AddBannerToLayout(){
+
         activity.runOnUiThread(new Runnable() {
-            @Override public void run() {
-                
-                //interstitial ad listener
-                FbInterstitialAd.setAdListener(new InterstitialAdListener() {
-                    @Override
-                    public void onInterstitialDisplayed(final Ad ad) {
-                        // Interstitial ad displayed callback
-                        Log.e(TAG, "Interstitial ad displayed.");
-                    }
-                    
-                    @Override
-                    public void onInterstitialDismissed(final Ad ad) {
-                        // Interstitial dismissed callback
-                        Log.e(TAG, "Interstitial ad dismissed.");
-                        FbInterstitialAd.loadAd(); // load the interstitial , so we can use it again.
-                    }
-                    @Override
-                    public void onError(final Ad ad, final AdError adError) {
-                        // Ad error callback
-                        Log.e(TAG, "Interstitial ad failed to load: " + adError.getErrorMessage());
-                    }
-        
-                    @Override
-                    public void onAdLoaded(final Ad ad) {
-                        // Interstitial ad is loaded and ready to be displayed
-                        Log.e(TAG, "Interstitial ad is loaded and ready to be displayed!");
-                        // switch lodaing signal to true
-                        FbInterstitialAd.show();
-                        
-                    }
-                    @Override
-                    public void onAdClicked(final Ad ad) {
-                        // Ad clicked callback
-                        Log.e(TAG, "Interstitial ad clicked!");
-                    }
-                    @Override
-                    public void onLoggingImpression(final Ad ad) {
-                        // Ad impression logged callback
-                        Log.e(TAG, "Interstitial ad impression logged!");
-                    }
-                }); 
-                FbInterstitialAd.loadAd();
+            @Override
+            public void run() {
+                linearLayout = new LinearLayout(activity);
+                linearLayoutParams =  new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                );
+                linearLayoutParams.setMargins(0,0,0,0);
+
+
+                windowManager = activity.getWindowManager();
+                windowManagerParams = new WindowManager.LayoutParams();
+                windowManagerParams.x = WindowManager.LayoutParams.MATCH_PARENT;
+                windowManagerParams.y = WindowManager.LayoutParams.MATCH_PARENT;
+                windowManagerParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+                windowManagerParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                windowManagerParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+                windowManagerParams.gravity = Gravity.BOTTOM;
+
+                linearLayout.addView(AdView,linearLayoutParams);
+                windowManager.addView(linearLayout,windowManagerParams);
+                isBannerShown = true;
+                Log.i(TAG, "Banner displaying !");
             }
         });
 
-                
     }
-
 
 }
--- src/facebookads.cpp
+++ src/facebookads.cpp
@@ -2,13 +2,13 @@
 #define EXTENSION_NAME FacebookAds
 #define LIB_NAME "FacebookAds"
 #define MODULE_NAME LIB_NAME
+#define DLIB_LOG_DOMAIN LIB_NAME
 
 // Defold SDK
-#define DLIB_LOG_DOMAIN LIB_NAME
 #include <dmsdk/sdk.h>
 #include <dmsdk/dlib/log.h>
 
-//#if defined(DM_PLATFORM_ANDROID)
+#if defined(DM_PLATFORM_ANDROID)
 
 
 static JNIEnv* Attach()
@@ -46,15 +46,25 @@ struct Facebook {
     {
         memset(this, 0, sizeof(*this));
     }
-
+    // void Update();
     jobject m_FB;
     jclass m_FacebookAdsClass;
-    jmethodID m_Public;
+    // jmethodID m_Public;
     jmethodID m_Initialize;
     jmethodID m_LoadInterstitial;
+    jmethodID m_LoadRewardedVideo;
+    jmethodID m_ShowInterstitial;
+    jmethodID m_ShowRewardedVideo;
+    jmethodID m_LoadBanner;
+    jmethodID m_ShowBanner;
+    jmethodID m_HideBanner;
 };
 
 }
+
+// Facebook::Update(){
+//     dmLogInfo("heh ........");
+// }
 
 static jclass GetClass(JNIEnv* env, const char* classname)
 {
@@ -83,33 +93,42 @@ static void InitFacebookExtension(){
     g_Facebook.m_FB = env->NewGlobalRef(env->NewObject(g_Facebook.m_FacebookAdsClass, jni_constructor, dmGraphics::GetNativeAndroidActivity()));
 
     
-    g_Facebook.m_Public = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "Public", "(Ljava/lang/String;)Ljava/lang/String;");
+    // g_Facebook.m_Public = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "Public", "(Ljava/lang/String;)Ljava/lang/String;");
     g_Facebook.m_Initialize = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "Initialize", "(Landroid/app/Activity;)V");
-    g_Facebook.m_LoadInterstitial = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "LoadInterstitial", "(Landroid/app/Activity;Ljava/lang/String;)V");
+    g_Facebook.m_LoadInterstitial = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "LoadInterstitial", "(Ljava/lang/String;)V");
+    g_Facebook.m_LoadRewardedVideo = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "LoadRewardedVideo", "(Ljava/lang/String;)V");
+    g_Facebook.m_LoadBanner = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "LoadBanner", "(Ljava/lang/String;)V");
+    g_Facebook.m_ShowInterstitial = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "ShowInterstitial", "()V");
+    g_Facebook.m_ShowRewardedVideo = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "ShowRewardedVideo", "()V");
+    g_Facebook.m_ShowBanner = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "ShowBanner", "()V");
+    g_Facebook.m_HideBanner = env->GetMethodID(g_Facebook.m_FacebookAdsClass, "HideBanner", "()V");
+   
     dmLogInfo("InitFacebookExtension ........");
 }
 
-static int Public(lua_State* L)
-{
-    AttachScope attachscope;
-    JNIEnv* env = attachscope.m_Env;
-    DM_LUA_STACK_CHECK(L, 1);
+// static int Public(lua_State* L)
+// {
+//     AttachScope attachscope;
+//     JNIEnv* env = attachscope.m_Env;
+//     DM_LUA_STACK_CHECK(L, 1);
 
-    const char* text_lua = luaL_checkstring(L,1);
-    jstring text = env->NewStringUTF(text_lua); // TODO : delete later
+//     const char* text_lua = luaL_checkstring(L,1);
+//     jstring text = env->NewStringUTF(text_lua); // TODO : delete later
 
-    jstring return_value = (jstring)env->CallObjectMethod(g_Facebook.m_FB , g_Facebook.m_Public , text);
-    lua_pushstring(L, env->GetStringUTFChars(return_value, 0));
-    env->DeleteLocalRef(return_value);
-    return 1;
-}
+//     jstring return_value = (jstring)env->CallObjectMethod(g_Facebook.m_FB , g_Facebook.m_Public , text);
+//     lua_pushstring(L, env->GetStringUTFChars(return_value, 0));
+
+//     // env->DeleteLocalRef(text);
+//     env->DeleteLocalRef(return_value);
+//     return 1;
+// }
 
 static int Initialize(lua_State* L){
     AttachScope attachscope;
     JNIEnv* env = attachscope.m_Env;
 
     env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_Initialize , dmGraphics::GetNativeAndroidActivity());
-    dmLogInfo("Initialize ........");
+    dmLogInfo("FB Ads Initialize ........");
     return 0;
 }
 
@@ -119,20 +138,88 @@ static int LoadInterstitial(lua_State* L){
     //DM_LUA_STACK_CHECK(L, 1);
 
     const char* lua_placement_id = luaL_checkstring(L,1);
-    jstring placement_id = env->NewStringUTF(lua_placement_id); // TODO : delete later
-    
-    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_LoadInterstitial , dmGraphics::GetNativeAndroidActivity() , placement_id);
-    dmLogInfo("LoadInterstitial ........");
+    jstring placement_id = env->NewStringUTF(lua_placement_id); 
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_LoadInterstitial , placement_id);
+    env->DeleteLocalRef(placement_id);
     return 0;
 }
+
+
+static int ShowInterstitial(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_ShowInterstitial);
+    return 0;
+}
+
+
+static int LoadRewardedVideo(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+    //DM_LUA_STACK_CHECK(L, 1);
+
+    const char* lua_placement_id = luaL_checkstring(L,1);
+    jstring placement_id = env->NewStringUTF(lua_placement_id); 
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_LoadRewardedVideo , placement_id);
+    env->DeleteLocalRef(placement_id);
+    return 0;
+}
+
+
+static int ShowRewardedVideo(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_ShowRewardedVideo);
+    return 0;
+}
+
+
+static int LoadBanner(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+    //DM_LUA_STACK_CHECK(L, 1);
+
+    const char* lua_placement_id = luaL_checkstring(L,1);
+    jstring placement_id = env->NewStringUTF(lua_placement_id); 
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_LoadBanner ,  placement_id);
+    env->DeleteLocalRef(placement_id);
+    return 0;
+}
+
+static int ShowBanner(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_ShowBanner);
+    return 0;
+}
+
+static int HideBanner(lua_State* L){
+    AttachScope attachscope;
+    JNIEnv* env = attachscope.m_Env;
+
+    env->CallVoidMethod(g_Facebook.m_FB , g_Facebook.m_HideBanner);
+    return 0;
+}
+
 
 
 // Functions exposed to Lua
 static const luaL_reg Module_methods[] =
 {
-    {"public", Public},
     {"initialize", Initialize},
     {"load_interstitial", LoadInterstitial},
+    {"show_interstitial", ShowInterstitial},
+    {"load_rewarded_video", LoadRewardedVideo},
+    {"show_rewarded_video", ShowRewardedVideo},
+    {"load_banner", LoadBanner},
+    {"show_banner", ShowBanner},
+    {"hide_banner", HideBanner},
     {0, 0}
 };
 
@@ -171,6 +258,28 @@ static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
     return dmExtension::RESULT_OK;
 }
 
-//#endif
+#else //if not defined(DM_PLATFORM_ANDROID) (other platfrom)
+
+static dmExtension::Result AppInitializeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result InitializeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result AppFinalizeExtension(dmExtension::AppParams* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+static dmExtension::Result FinalizeExtension(dmExtension::Params* params)
+{
+    return dmExtension::RESULT_OK;
+}
+
+#endif // if defined(DM_PLATFORM_ANDROID)
 
 DM_DECLARE_EXTENSION(EXTENSION_NAME, LIB_NAME, AppInitializeExtension, AppFinalizeExtension, InitializeExtension, 0, 0, FinalizeExtension)
